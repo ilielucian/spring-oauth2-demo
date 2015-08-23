f30a16226ee59c336c1af535169d10e5a827cc3d
Lucian Ilie <ilielucian@outlook.com>
2015-08-23 13:54:53 +0300

    basic config of OAuth2 Resource Server

--- src/main/java/ilielucian/demo/bank/authorization/config/OAuth2ServerConfig.java
+++ src/main/java/ilielucian/demo/bank/authorization/config/OAuth2ServerConfig.java
@@ -2,6 +2,7 @@ package ilielucian.demo.bank.authorization.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -13,18 +14,30 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @Configuration
 class OAuth2ServerConfig {
 
-//    @Configuration
-//    @EnableResourceServer
-//    static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-//
-//        @Override
-//        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//            super.configure(resources);
-//        }
-//
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            super.configure(http);
-//        }
-//    }
+    private static final String BANK_RESOURCE_ID = "bank";
+
+    @Configuration
+    @EnableResourceServer
+    static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.resourceId(BANK_RESOURCE_ID).stateless(false);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .and()
+                    .requestMatchers()
+                        .antMatchers("/admin/**", "/bankaccount/**", "/oauth/users/**", "/oauth/clients/**")
+                    .and()
+                    .authorizeRequests()
+                        .antMatchers("/admin/**")
+                            .access("#oauth2.hasScope('read')")
+                        .antMatchers("/bankaccount/**")
+                            .access("#oauth2.hasScope('trust') or (!#oauth2.isOAuth() and hasRole('ROLE_ADMIN'))");
+        }
+    }
 }
