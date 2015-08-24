6ce381fde3fafacd86585d022b543a91206f838d
Lucian Ilie <ilielucian@outlook.com>
2015-08-24 09:03:07 +0300

    added OAuth authorization server stub

--- src/main/java/ilielucian/demo/bank/authorization/config/OAuth2ServerConfig.java
+++ src/main/java/ilielucian/demo/bank/authorization/config/OAuth2ServerConfig.java
@@ -1,11 +1,23 @@
 package ilielucian.demo.bank.authorization.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
  *
@@ -37,7 +49,57 @@ class OAuth2ServerConfig {
                         .antMatchers("/admin/**")
                             .access("#oauth2.hasScope('read')")
                         .antMatchers("/bankaccount/**")
-                            .access("#oauth2.hasScope('trust') or (!#oauth2.isOAuth() and hasRole('ROLE_ADMIN'))");
+                            .access("#oauth2.hasScope('trust') or (!#oauth2.isOAuth() and hasRole('ROLE_ADMIN'))")
+                    /*.regexMatchers(HttpMethod.DELETE, "/oauth/users/([^/].*?)/tokens/.*")
+                    .access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('write')")
+                    .regexMatchers(HttpMethod.GET, "/oauth/clients/([^/].*?)/users/.*")
+                    .access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('read')")
+                    .regexMatchers(HttpMethod.GET, "/oauth/clients/.*")
+                    .access("#oauth2.clientHasRole('ROLE_CLIENT') and #oauth2.isClient() and #oauth2.hasScope('read')")*/;
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Bean
+        TokenStore tokenStore() {
+            return new InMemoryTokenStore();
+        }
+
+        @Bean
+        ApprovalStore approvalStore() {
+            TokenApprovalStore store = new TokenApprovalStore();
+            store.setTokenStore(tokenStore());
+
+            return store;
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore());
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("bank-client")
+                    .resourceIds(BANK_RESOURCE_ID)
+                    .authorizedGrantTypes("authorization_code", "implicit")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read")
+                    .and()
+                    .withClient("bank-client-postman")
+                    .resourceIds(BANK_RESOURCE_ID)
+                    .authorizedGrantTypes("implicit")
+                    .authorities("ROLE_TRUSTED_CLIENT")
+                    .scopes("read", "write")
+                    .secret("secret")
+                    .redirectUris("https://www.getpostman.com/oauth2/callback");
         }
     }
 }
